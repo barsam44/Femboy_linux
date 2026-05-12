5d05dce61c26bac065535953536762a9e2d615d1
barsam <150834503+barsam44@users.noreply.github.com>
2026-05-12 19:03:19 +0330

    Delete model_chunks directory

--- model_chunks/merge.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Merging model.gguf...
-copy /b model.gguf.part_* ..\model.gguf
-echo Done! model.gguf is in the parent folder.
--- model_chunks/merge.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Merging model.gguf..."
-cat model.gguf.part_* > ../model.gguf
-echo "Done! model.gguf is in the parent folder."
